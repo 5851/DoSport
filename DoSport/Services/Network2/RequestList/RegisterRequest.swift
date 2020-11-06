f60ca2cae32936da8e10257f4245152803cef2ea
2jumper3 <2jumper3@gmail.com>
2020-11-06 17:50:00 +0300

    добавил комментарии для работы с сетью

--- DoSport/Services/Network2/RequestList/RegisterRequest.swift
+++ DoSport/Services/Network2/RequestList/RegisterRequest.swift
@@ -27,8 +27,9 @@ class RegisterRequest: AbstractRequestFactory {
         self.queue = queue
     }
 }
-
+//MARK: - забиваем даные
 extension RegisterRequest: RegisterRequestFactory {
+    //в @escaping вставляем модель из responses в каком формате оно выйдет легко проверить через приложение insomnia
     func register(firstname: String, lastname: String, password: String, passwordConfirm: String, username: String, completionHandler: @escaping (AFDataResponse<RegisterResult>) -> Void) {
         let requestModel = Register(baseUrl: baseUrl, firstname: firstname, lastname: lastname, password: password, passwordConfirm: passwordConfirm, username: username)
         self.request(request: requestModel, completionHandler: completionHandler)
@@ -39,13 +40,13 @@ extension RegisterRequest {
     struct Register: RequestRouter {
         let baseUrl: URL
         let method: HTTPMethod = .post
-        let path: String = "profile"
+        let path: String = "profile" //адрес послеbaseUrl
+        //все переменные которые уходят на сервер
         let firstname: String
         let lastname: String
         let password: String
         let passwordConfirm: String
         let username: String
-        
         var parameters: Parameters? {
             return [
                 "firstName": firstname,
